5eb07f918f77f41874f08cc9d049a71d04697060
kumarece1305 <110160805+kumarece1305@users.noreply.github.com>
2022-07-30 08:45:07 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "s3"{
-        bucket ="new-bucket-vishanth"
-        key = "lab/terraform.tfstate"
-        region = "ca-central-1"
-        dynamodb_table = "new_table"
-    }
-}
